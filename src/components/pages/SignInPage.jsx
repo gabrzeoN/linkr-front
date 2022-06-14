f7bec5113dfb595e23298bdef9d2eab41db4e859
Gabriel Cari <g.cari@hotmail.com>
2022-06-14 14:26:03 -0300

    style: mobile layout done.

--- src/components/pages/SignInPage.jsx
+++ src/components/pages/SignInPage.jsx
@@ -154,92 +154,60 @@ const Main = styled.main`
         }
     }
 
-    /* @media (min-width: 400px) {
+    @media (max-width: 1450px) {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
         .logo{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        padding-left: 144px;
-        height: 100%;
-        width: 70%;
-        background: #151515;
-        box-shadow: 4px 0px 4px rgba(0, 0, 0, 0.25);
-        color: white;
-        z-index: 1;
-        h1{
-            font-family: 'Passion One';
-            font-weight: 700;
-            font-size: 106px;
-            line-height: 117px;
-            letter-spacing: 0.05em;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+            height: 175px;
+            padding: 10px 0px 27px 0px;
+            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            h1{
+                font-size: 76px;
+                line-height: 84px;
+                letter-spacing: 0.05em;
+            }
+            p{ 
+                font-size: 23px;
+                line-height: 34px;
+                text-align: center;
+                width: 237px;
+            }
         }
-        p{
-            
-            font-family: 'Oswald';
-            font-weight: 700;
-            font-size: 43px;
-            line-height: 64px;
-            width: 442px;
+        .inputs{
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: center;
+            width: 100%;
+            input{
+                :first-child{
+                    margin-top: 40px;
+                }
+                width: 330px;
+                height: 55px;
+                font-size: 22px;
+                line-height: 33px; 
+                ::placeholder{
+                    font-size: 22px;
+                    line-height: 33px; 
+                }
+            }
+            button{
+                font-size: 22px;
+                line-height: 33px;
+                width: 330px;
+                height: 55px;
+            }
+            a{
+                font-size: 17px;
+                line-height: 20px;
+            }
         }
     }
-    .inputs{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        width: 30%;
-        background: #333333;
-        color: white;
-        input{
-            display: flex;
-            width: 429px;
-            height: 65px;
-            border: 0px;
-            border-radius: 6px;
-            padding-left: 17px;
-            font-family: 'Oswald';
-            font-weight: 700;
-            font-size: 27px;
-            line-height: 40px;
-            color: #9F9F9F;
-            margin-bottom: 13px;
-            ::placeholder{
-                font-family: 'Oswald';
-                font-weight: 700;
-                font-size: 27px;
-                line-height: 40px;
-                color: #9F9F9F;
-            }
-        }
-        button{
-            width: 429px;
-            height: 65px;
-            border: 0px;
-            border-radius: 6px;
-            padding-left: 17px;
-            font-family: 'Oswald';
-            font-weight: 700;
-            font-size: 27px;
-            line-height: 40px;
-            color: white;
-            margin-bottom: 22px;
-            background: #1877F2;
-            border-radius: 6px;
-            :hover{
-                background: #1256af;
-            }
-        }
-        a{
-            font-family: 'Lato';
-            font-size: 20px;
-            line-height: 24px;
-            text-decoration-line: underline;
-            color: white;
-        }
-    } */
 `;
