82dae97c86c892e0753d4b1ce72e65ac7702a939
joltzz <guijsilva02@gmail.com>
2022-06-14 16:15:35 -0300

    feat: created sing up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,19 @@ import "../assets/css/reset.css";
 import "../assets/css/style.css";
 import UserContext from "../contexts/UserContext";
 
-import SignInPage from "./pages/SignInPage.jsx";
+import SignInPage from "./pages/Sing-in/SignInPage.jsx";
+import SignUp from "./pages/Sign-up";
 
 export default function App(){
+    const [userData, setUserData] = useState(null);
     return(
-        // <UserContext.Provider value={ } >
+        <UserContext.Provider value={ {userData, setUserData} } >
             <BrowserRouter>
                 <Routes>
-                        <Route path="/" element={<SignInPage />} />
+                        <Route path="/" element={<SignInPage/>} />
+                        <Route path="/sign-up" element={<SignUp/>} />
                 </Routes>
             </BrowserRouter>
-        // </UserContext.Provider>
+        </UserContext.Provider>
     );
 }
