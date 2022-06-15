edd7bab8d55936496323677aa5116537e4b84fd3
Gabriel Cari <g.cari@hotmail.com>
2022-06-15 13:17:21 -0300

    feat: logout done.

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const BASE_URL=localStorage;
+const BASE_URL="http://localhost:5000";
 
 async function signUp(formData) {
     const promise = await axios.post(`${BASE_URL}/sign-up`, formData)
-
     return promise;
 }
-
 
 const api={
     signUp,
