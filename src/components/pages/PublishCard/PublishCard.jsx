8e9a09dfb3d51e3168a811dd1ccae98fc3f08fb5
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-06-15 17:19:37 -0300

    feat: add publish card

--- src/components/pages/PublishCard/PublishCard.jsx
+++ src/components/pages/PublishCard/PublishCard.jsx
@@ -3,26 +3,32 @@ import imagePerfil from "./../../../assets/img/image 4.png";
 import { useState } from "react";
 
 export default function PublishCard(){
-    const [post, setPost] = useState({});
+    const [post, setPost] = useState({url: "", message:""});
     const [disabled, setDisabled] = useState(false);
 
-    const token = ""; //TODO:pegar token do localStorage
-    const config = {headers: {Authorization: `Bearer ${token}`}};
+    // const token = ""; //TODO:pegar token do localStorage
+    // const config = {headers: {Authorization: `Bearer ${token}`}};
 
     function submitPublish(e){
         e.preventDefault();
         setDisabled(true);
-
         console.log("post", post);
+        setDisabled(false);
+        setPost({url:"", message:""});
 
         //TODO:fazer o deploy do banco e definir o objeto que será enviado
-        const URL = "http://localhost:5000/posts"
-        const promise = axios.post(URL, post, config);
-        promise.then(() => console.log("post enviado"));
-        promise.catch((erro) => {
-            console.log("erro", erro);
-            setDisabled(false);
-        });
+        // const URL = "http://localhost:5000/posts";
+        // const promise = axios.post(URL, post, config);
+        // promise.then(() => {
+        //     console.log("post enviado", post);
+        //     setDisabled(false);
+        //     setPost({url:"", message:""});
+        //     console.log("campo vazio", post);
+        // });
+        // promise.catch((erro) => {
+        //     console.log("erro", erro);
+        //     setDisabled(false);
+        // });
 
     }
 
@@ -40,8 +46,8 @@ export default function PublishCard(){
 
                     <Text placeholder="Awesome article about #javascript"
                         type="text"
-                        value={post.text}
-                        onChange={(e) => setPost({...post, text:e.target.value})}
+                        value={post.message}
+                        onChange={(e) => setPost({...post, message:e.target.value})}
                         disabled={disabled}/>
 
                     <Buttons type="submit" disabled={disabled}> {!disabled ? "Publish" : "Publishing..."}</Buttons>
